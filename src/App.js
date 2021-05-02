dc00584a7e5aac48622049df958cab9e0b5df8dd
Sibghat99 <54533679+Sibghat99@users.noreply.github.com>
2021-05-02 10:05:42 +0600

    First Upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1><img src= "logo.png" alt="my image"/></h1>
       </header>
-    </div>
+      <text>Categories:</text>
+      <br></br>
+      <br></br>
+      <button>Processors</button>
+      <br></br>
+      <button>Graphics Card</button>
+      <br></br>
+      <button>RAM</button>
+      <br></br>
+      <button>Hard Drive and SSD</button>
+      <br></br>
+      <button>Monitor</button>
+      <br></br>
+      <button>Power Supply</button>
+      <br></br>
+      <button>Motherboard</button>
+      <br></br>
+      <button>Accessories</button>
+      <br></br>
+      <br></br>
+      <text>Quick ShortCuts</text>
+      <br></br>
+      <br></br>
+      <button>GPU<br></br><img src="PSX_20210424_235421.jpg" alt="my image"  /></button>
+      &nbsp;&nbsp;&nbsp;
+      <button>CPU<br></br><img src="PSX_20210425_001231.jpg" alt="my image"  /></button>
+      &nbsp;&nbsp;&nbsp;
+      <button>Accessories<br></br><img src="PSX_20210425_001625.jpg" alt="my image"  /></button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <button>Recommended Shops<br></br><img src="shops.jpg" alt="my image"/></button>
+      </div>
+    
   );
 }
 
