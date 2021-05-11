791e883547a9307216093f874a28ad950a3397f5
SayedBinHarun <79776759+SayedBinHarun@users.noreply.github.com>
2021-05-12 00:21:10 +0600

    Add files via upload

---
+++ techhub.sql
@@ -0,0 +1,493 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 07, 2021 at 07:38 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `techhub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accessories`
+--
+
+CREATE TABLE `accessories` (
+  `AccId` int(8) NOT NULL,
+  `Model` varchar(50) NOT NULL,
+  `Price` int(10) NOT NULL,
+  `ShopName` varchar(20) DEFAULT NULL,
+  `ReviewLink` varchar(2083) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `accessories`
+--
+
+INSERT INTO `accessories` (`AccId`, `Model`, `Price`, `ShopName`, `ReviewLink`) VALUES
+(9001, 'WALTON WKS004WN KEYBOARD', 240, 'Skyland', 'null'),
+(9002, 'RK ROYAL KLUDGE RK71 RGB KEYBOARD (BLACK)', 3999, 'TechLand', 'https://techbroll.com/2019/04/royal-kludge-rk71-mechanical-keyboard-review.html#:~:text=Design%20and%20Construction,have%20the%20royal%20kludge%20logo.'),
+(9003, 'Razer Huntsman Elite Opto-Mechanical Keyboard', 16300, 'StarTech', 'https://www.youtube.com/watch?v=sVZXxbdLn7Q'),
+(9004, 'WALTON WMS025WN WIRED MOUSE', 175, 'TechLand', 'null'),
+(9005, 'RAZER VIPER MINI ULTRA-LIGHTWEIGHT GAMING MOUSE', 2299, 'Skyland', 'https://www.youtube.com/watch?v=esQhZd31Jt4'),
+(9006, 'Razer Viper Ultimate RGB Wireless Gaming Mouse', 10300, 'StarTech', 'https://www.rtings.com/mouse/reviews/razer/viper-ultimate');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `Fname` varchar(8) DEFAULT NULL,
+  `Lname` varchar(8) DEFAULT NULL,
+  `Admin_ID` int(8) NOT NULL,
+  `AdminPassword` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`Fname`, `Lname`, `Admin_ID`, `AdminPassword`) VALUES
+('Sibghat', 'Shaikh', 101, '123456'),
+('Sayed', 'Harun', 102, '123456'),
+('Newaz', 'Sharif', 103, '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `benchmark_gpu`
+--
+
+CREATE TABLE `benchmark_gpu` (
+  `GpuId` int(11) NOT NULL,
+  `BenchmarkValue` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `benchmark_gpu`
+--
+
+INSERT INTO `benchmark_gpu` (`GpuId`, `BenchmarkValue`) VALUES
+(3001, 3.4),
+(3002, 12.9),
+(3003, 14.7),
+(3004, 22.7),
+(3005, 31),
+(3006, 43.2),
+(3007, 46.5),
+(3008, 60.1),
+(3009, 70.7),
+(3010, 76.2),
+(3011, 89),
+(3012, 131),
+(3013, 87),
+(3014, 118),
+(3015, 153);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `benchmark_processor`
+--
+
+CREATE TABLE `benchmark_processor` (
+  `ProcessorId` int(11) NOT NULL,
+  `BenchmarkValue` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `benchmark_processor`
+--
+
+INSERT INTO `benchmark_processor` (`ProcessorId`, `BenchmarkValue`) VALUES
+(4001, 71),
+(4002, 79.8),
+(4003, 84),
+(4004, 85.3),
+(4005, 87.8),
+(4006, 97.5),
+(4007, 98.4),
+(4008, 97.5),
+(4009, 102),
+(4010, 76.5),
+(4011, 84.6),
+(4012, 83.4),
+(4013, 96.5),
+(4014, 87),
+(4015, 99.1),
+(4016, 101);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `Serial` int(8) NOT NULL,
+  `CategoryName` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`Serial`, `CategoryName`) VALUES
+(1, 'Processor'),
+(2, 'Graphics Card'),
+(3, 'RAM'),
+(4, 'Hard Drive and SSD'),
+(5, 'Monitor'),
+(6, 'Power Supply'),
+(7, 'Motherboard'),
+(8, 'Accessories');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gpu`
+--
+
+CREATE TABLE `gpu` (
+  `GpuId` int(8) NOT NULL,
+  `Model` varchar(100) NOT NULL,
+  `Price` int(10) NOT NULL,
+  `ShopName` varchar(20) DEFAULT NULL,
+  `ReviewLink` varchar(2083) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `gpu`
+--
+
+INSERT INTO `gpu` (`GpuId`, `Model`, `Price`, `ShopName`, `ReviewLink`) VALUES
+(3001, 'COLORFUL GEFORCE GT710-2GD3-V 2GB GRAPHICS CARD', 3900, 'TechLand', 'https://www.youtube.com/watch?v=r3-_nYkYWFY'),
+(3002, 'BIOSTAR RADEON RX550 2GB GDDR5 GRAPHICS CARD', 7000, 'TechLand', 'https://www.amazon.com/product-reviews/B0887VC9HK?reviewerType=all_reviews'),
+(3003, 'ZOTAC GeForce GT 1030 Low Profile 2GB GDDR5 Graphics Card', 7300, 'StarTech', 'https://www.notebookcheck.net/Zotac-GeForce-GT-1030-Review.229134.0.html'),
+(3004, 'XFX RADEON RX 560 4GB GRAPHICS CARD', 10200, 'TechLand', 'https://versus.com/en/xfx-radeon-rx-560-single-fan-oc-4gb'),
+(3005, 'COLORFUL IGAME GEFORCE GTX 1050TI VULCAN U 4GB GRAPHICS CARD', 14300, 'TechLand', 'https://tech4gamers.com/colorful-igame-geforce-gtx-1050ti-vulkan-u-4g-ec/'),
+(3006, 'ZOTAC GAMING GEFORCE GTX 1650 OC 4GB GRAPHICS CARD', 13800, 'TechLand', 'https://www.guru3d.com/articles-pages/zotac-geforce-gtx-1650-gaming-4gb-review,1.html'),
+(3007, 'Sapphire Pulse Radeon RX 570 4GB GDDR5 HDMI DP Graphics card', 12700, 'StarTech', 'https://hexus.net/tech/reviews/graphics/108460-sapphire-radeon-rx-570-pulse-oc-4gb/'),
+(3008, 'MSI GeForce GTX 1650 Super Ventus XS OC 4GB Graphics Card', 16500, 'StarTech', 'https://versus.com/en/msi-geforce-gtx-1650-super-ventus-xs-oc'),
+(3009, 'ZOTAC GAMING GEFORCE GTX 1660 SUPER TWIN FAN 6GB GRAPHICS CARD', 44000, 'TechLand', 'https://www.pcmag.com/reviews/zotac-geforce-gtx-1660-super-twin-fan'),
+(3010, 'Colorful iGame GeForce 1660 TI ULTRA 6GB-V Graphics Card', 27500, 'StarTech', 'https://versus.com/en/colorful-igame-geforce-gtx-1660-ti-ultra'),
+(3011, 'ZOTAC GAMING GEFORCE RTX 2060 AMP 6GB GRAPHICS CARD', 50500, 'TechLand', 'https://www.pcmag.com/reviews/zotac-geforce-rtx-2060-amp'),
+(3012, 'GALAX GEFORCE RTX 3060 1-CLICK OC 12GB GDDR6 GRAPHICS CARD', 65000, 'StarTech', 'https://www.youtube.com/watch?v=M4PVeM50wL8'),
+(3013, 'GIGABYTE RADEON RX 5600 XT GAMING OC 6GB GRAPHICS CARD', 52000, 'TechLand', 'https://www.tomshardware.com/reviews/gigabyte-rx-5600-xt-gaming-oc-6g'),
+(3014, 'Sapphire PULSE AMD Radeon RX 6700 XT 12GB RDNA GDDR6 Graphics Card', 82900, 'StarTech', 'https://www.pcgamer.com/sapphire-nitro-plus-radeon-rx-6700-xt-review-benchmarks/'),
+(3015, 'Gigabyte GeForce RTX 3070 Gaming OC 8GB Graphics Card', 100000, 'TechLand', 'https://www.techpowerup.com/review/gigabyte-geforce-rtx-3070-gaming-oc/');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hddandssd`
+--
+
+CREATE TABLE `hddandssd` (
+  `DriveId` int(8) NOT NULL,
+  `Model` varchar(50) NOT NULL,
+  `Price` int(10) NOT NULL,
+  `ShopName` varchar(20) DEFAULT NULL,
+  `ReviewLink` varchar(2083) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `hddandssd`
+--
+
+INSERT INTO `hddandssd` (`DriveId`, `Model`, `Price`, `ShopName`, `ReviewLink`) VALUES
+(7001, 'TOSHIBA 1TB 7200 RPM SATA HARD DISK', 3500, 'TechLand', NULL),
+(7002, 'Toshiba 2TB Sata Desktop Hard Disk', 5070, 'StarTech', NULL),
+(7003, 'TOSHIBA 3TB SATA DESKTOP HARD DISK', 6700, 'TechLand', NULL),
+(7004, 'WESTERN DIGITAL WD40PURZ 4TB PURPLE', 7999, 'TechLand', NULL),
+(7005, 'RAMSTA R600 M.2 2280 128GB SATA3 SSD', 1799, 'TechLand', NULL),
+(7006, 'TWINMOS 256GB M.2 SSD', 2800, 'Skyland', NULL),
+(7007, 'TwinMOS TW300 512GB M.2 SSD', 4950, 'StarTech', NULL),
+(7008, 'NETAC N535N 1TB M.2 2280 SSD', 8700, 'TechLand', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `monitor`
+--
+
+CREATE TABLE `monitor` (
+  `MonitorId` int(8) NOT NULL,
+  `Model` varchar(100) NOT NULL,
+  `Price` int(10) NOT NULL,
+  `ShopName` varchar(20) DEFAULT NULL,
+  `ReviewLink` varchar(2083) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `monitor`
+--
+
+INSERT INTO `monitor` (`MonitorId`, `Model`, `Price`, `ShopName`, `ReviewLink`) VALUES
+(10001, 'ACER K202HQL 19.5 Inch 720p LED Monitor', 5000, 'StarTech', 'null'),
+(10002, 'AOC 22B1HS 21.5 INCH FHD SLIM BORDERLESS LED IPS MONITOR', 8900, 'TechLand', 'null'),
+(10003, 'HP X24IH 23.8 INCH 144HZ LED FHD FREESYNC IPS GAMING MONITOR', 22500, 'TechLand', 'https://www.rtings.com/monitor/reviews/hp/x24ih'),
+(10004, 'XIAOMI XMMNT27HQ 27-INCH IPS QHD 165HZ GAMING MONITOR', 36300, 'TechLand', 'https://www.youtube.com/watch?v=PCebWiIg0QI'),
+(10005, 'Xiaomi Mi 34\" 144Hz FreeSync Curved Monitor', 42000, 'StarTech', 'https://www.youtube.com/watch?v=1hMAw8xCd0Y'),
+(10006, 'LG 27GN950-B 27\" 4K UHD Nano IPS Ultragear 144Hz G-SYNC Gaming Monitor', 107500, 'StarTech', 'https://www.youtube.com/watch?v=55pUgm6V4_Q');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `motherboard`
+--
+
+CREATE TABLE `motherboard` (
+  `MotherboardId` int(8) NOT NULL,
+  `Model` varchar(50) NOT NULL,
+  `Price` int(10) NOT NULL,
+  `ShopName` varchar(20) DEFAULT NULL,
+  `ReviewLink` varchar(2083) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `motherboard`
+--
+
+INSERT INTO `motherboard` (`MotherboardId`, `Model`, `Price`, `ShopName`, `ReviewLink`) VALUES
+(6001, 'MSI H310M-PRO-VD MICRO-ATX MOTHEBOARD', 6100, 'TechLand', 'https://pangoly.com/en/review/msi-h310m-pro-vd'),
+(6002, 'ASROCK H410M-HDV 10th gen motherboard', 7100, 'TechLand', 'https://versus.com/en/asrock-h410m-hdv'),
+(6003, 'Biostar b365mhc', 6300, 'Skyland', 'https://www.techpowerup.com/257432/biostar-launches-the-b365mhc-micro-atx-motherboard'),
+(6004, 'MSI B460M PRO 10TH GEN INTEL MOTHERBOARD', 7700, 'TechLand', 'https://pangoly.com/en/review/msi-b460m-pro'),
+(6005, 'Gigabyte Intel Z390 D Advanced Thermal Design ATX ', 9000, 'StarTech', 'https://versus.com/en/gigabyte-z390-d'),
+(6006, 'MSI Z490-A PRO 10th Gen Intel ATX Motherboard', 15900, 'StarTech', 'https://www.amazon.com/product-reviews/B0876HPVHX'),
+(6007, 'MSI A520M-A Pro', 6700, 'StarTech', 'https://versus.com/en/msi-a520m-a-pro'),
+(6008, 'GIGABYTE B450M H ULTRA DURABLE', 6300, 'TechLand', 'https://versus.com/en/gigabyte-b450m-h'),
+(6009, 'MSI B550M-A PRO AM4', 9200, 'StarTech', 'https://www.youtube.com/watch?v=Qf0-JoQLeJo'),
+(6010, 'GIGABYTE X570 Ud', 14500, 'SkyLand', 'https://tiny-reviews.com/reviews/1798-review-on-gigabyte-x570-ud-motherboard-am4-atx');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `processor`
+--
+
+CREATE TABLE `processor` (
+  `ProcessorId` int(8) NOT NULL,
+  `Model` varchar(100) NOT NULL,
+  `Price` int(10) NOT NULL,
+  `ShopName` varchar(30) DEFAULT NULL,
+  `ReviewLink` varchar(2083) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `processor`
+--
+
+INSERT INTO `processor` (`ProcessorId`, `Model`, `Price`, `ShopName`, `ReviewLink`) VALUES
+(4001, 'Intel Pentium Gold G6400 10th gen Coffee Lake Processor', 5800, 'TechLand', 'https://www.pcmag.com/reviews/intel-pentium-gold-g6400'),
+(4002, 'INTEL CORE I3 9100F 9TH GEN PROCESSOR', 7300, 'TechLand', 'https://www.legitreviews.com/intel-core-i3-9100-4-core-processor-review_213639'),
+(4003, 'INTEL CORE I3-10100 10TH GEN PROCESSOR', 12500, 'TechLand', 'https://www.pcmag.com/reviews/intel-core-i3-10100'),
+(4004, 'INTEL CORE I5 9400F 9TH GEN PROCESSOR', 12500, 'StarTech', 'https://www.tomshardware.com/reviews/intel-core-i5-9400f-cpu-integrated-graphics,6107.html'),
+(4005, 'Intel 10th Gen Core i5-10400F Processor', 13900, 'StarTech', 'https://www.techpowerup.com/review/intel-core-i5-10400f/'),
+(4006, 'INTEL CORE I7-9700K 9TH GEN PROCESSOR', 30400, 'Skyland', 'https://www.pcmag.com/reviews/intel-core-i7-9700k'),
+(4007, 'INTEL CORE I7-10700K OCTA CORE 10TH GENERATION PROCESSOR', 31800, 'TechLand', 'https://www.windowscentral.com/intel-core-i7-10700k-review'),
+(4008, 'INTEL CORE I9-9900K 9TH GENERATION PROCESSOR', 34000, 'TechLand', 'https://www.techradar.com/reviews/intel-core-i9-9900k-review'),
+(4009, 'INTEL CORE I9-10900K 10TH GEN 10 CORE PROCESSOR', 45000, 'TechLand', 'https://www.tomshardware.com/reviews/intel-core-i9-10900k-cpu-review'),
+(4010, 'AMD Ryzen 3 3100 Desktop Processor With Wraith Stealth Cooling Solution', 11500, 'StarTech', 'https://www.techradar.com/reviews/amd-ryzen-3-3100'),
+(4011, 'AMD RYZEN 5 3500X 6 CORE 6 THREAD PROCESSOR', 13600, 'Skyland', 'https://www.techspot.com/review/1966-amd-ryzen-5-3500x/'),
+(4012, 'AMD RYZEN 5 3600 6 CORE 6 THREAD PROCESSOR', 16500, 'TechLand', 'https://www.tomshardware.com/reviews/amd-ryzen-5-3600-review,6287.html'),
+(4013, 'AMD RYZEN 5 5600X PROCESSOR 3.7 GHZ UP TO 4.6GHZ SIX-CORE AM4 PROCESSOR', 32000, 'TechLand', 'https://www.tomshardware.com/reviews/amd-ryzen-5-5600x-zen-3-review'),
+(4014, 'AMD Ryzen 7 3700X Processor', 27500, 'StarTech', 'https://www.techradar.com/reviews/amd-ryzen-7-3700x'),
+(4015, 'AMD RYZEN 7 5800X PROCESSOR', 43500, 'StarTech', 'https://www.techradar.com/reviews/amd-ryzen-7-5800x'),
+(4016, 'AMD RYZEN 9 5900X PROCESSOR', 57500, 'Skyland', 'https://www.techradar.com/reviews/amd-ryzen-9-5900x');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `psu`
+--
+
+CREATE TABLE `psu` (
+  `PsuId` int(8) NOT NULL,
+  `Model` varchar(100) NOT NULL,
+  `Price` int(10) NOT NULL,
+  `ShopName` varchar(20) DEFAULT NULL,
+  `ReviewLink` varchar(2083) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `psu`
+--
+
+INSERT INTO `psu` (`PsuId`, `Model`, `Price`, `ShopName`, `ReviewLink`) VALUES
+(5001, 'ANTEC ATOM 350W 350 WATT POWER SUPPLY', 2250, 'TechLand', 'https://www.youtube.com/watch?v=N_ZkZ801bC0'),
+(5002, 'DEEPCOOL DN450 450W 80 PLUS 230V POWER SUPPLY', 3050, 'SkyLand', 'https://uae.microless.com/product/deepcool-dn450-450w-80-plus-230v-eu-certified-atx-power-supply-dp-230eu-dn450/'),
+(5003, 'DEEPCOOL DA500 500W 80 PLUS BRONZE POWER SUPPLY', 4000, 'TechLand', 'https://www.eteknix.com/deep-cool-da500-aurora-500w-power-supply-review/'),
+(5004, 'GIGABYTE P550B 550W 80 PLUS BRONZE CERTIFIED POWER SUPPLY', 3999, 'TechLand', 'https://www.funkykit.com/reviews/power-supplies/gigabyte-p550b-power-supply-review/'),
+(5005, 'Montech Beta 650w 80 plus Bronze psu', 5100, 'TechLand', 'https://www.amazon.com/Montech-Certified-Japanese-Capacitors-Continuous/product-reviews/B084Q9NH11?reviewerType=all_reviews'),
+(5006, 'ANTEC NEOECO NE700G ZEN 700W 80PLUS Gold', 7700, 'TechLand', 'https://www.thefpsreview.com/2020/11/11/antec-neo-eco-gold-zen-700w-power-supply-review/'),
+(5007, 'Cooler Master MWE 750W V2 Non-Modular 80 Plus Bronze Certified Power Supply', 7700, 'StarTech', 'https://www.eteknix.com/cooler-master-mwe-gold-750-v2-power-supply-review/'),
+(5008, 'DEEPCOOL DQ850-M-V2L 850W 80 PLUS GOLD FULL MODULAR POWER SUPPLY', 9500, 'TechLand', 'https://www.tomshardware.com/reviews/deepcool-gamerstorm-dq-m-v2l-850w-power-supply-review');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ram`
+--
+
+CREATE TABLE `ram` (
+  `RamId` int(8) NOT NULL,
+  `Model` varchar(50) NOT NULL,
+  `Price` int(10) NOT NULL,
+  `ShopName` varchar(20) DEFAULT NULL,
+  `ReviewLink` varchar(2083) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `ram`
+--
+
+INSERT INTO `ram` (`RamId`, `Model`, `Price`, `ShopName`, `ReviewLink`) VALUES
+(8001, 'GEIL PRISTINE 4GB DDR4 2400MHZ', 1900, 'TechLand', NULL),
+(8002, 'TRANSCEND 4GB DDR4 2666MHZ', 2300, 'StarTech', NULL),
+(8003, 'RAMSTA 8GB DDR4 2400MHZ', 2800, 'TechLand', NULL),
+(8004, 'APACER 8GB DDR4 2666MHZ', 2900, 'TechLand', NULL),
+(8005, 'THERMALTAKE H-ONE 8GB DDR4 3000MHZ', 3400, 'Skyland', NULL),
+(8006, 'G.SKILL TRIDENT Z 4GB DDR4 3200MHZ', 4000, 'TechLand', NULL),
+(8007, 'Team Elite Plus 16GB DDR4 2400MHz', 5500, 'StarTech', NULL),
+(8008, 'Transcend 16GB DDR4 2666Mhz', 8000, 'StarTech', NULL),
+(8009, 'TEAM T-FORCE DARK 16GB DDR4 3200MHZ', 8000, 'TechLand', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recommendedshops`
+--
+
+CREATE TABLE `recommendedshops` (
+  `ShopId` int(8) NOT NULL,
+  `Name` varchar(25) NOT NULL,
+  `Link` varchar(2083) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `recommendedshops`
+--
+
+INSERT INTO `recommendedshops` (`ShopId`, `Name`, `Link`) VALUES
+(201, 'StarTech', 'https://www.startech.com.bd/'),
+(202, 'TechLand', 'https://www.techlandbd.com/'),
+(203, 'Ryans', 'https://www.ryanscomputers.com/'),
+(204, 'UCC', 'https://ucc-bd.com/'),
+(205, 'SkyLand', 'https://www.skyland.com.bd/'),
+(206, 'GlobalBrand', 'https://www.globalbrand.com.bd/');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accessories`
+--
+ALTER TABLE `accessories`
+  ADD PRIMARY KEY (`AccId`);
+
+--
+-- Indexes for table `benchmark_gpu`
+--
+ALTER TABLE `benchmark_gpu`
+  ADD KEY `GpuId` (`GpuId`);
+
+--
+-- Indexes for table `benchmark_processor`
+--
+ALTER TABLE `benchmark_processor`
+  ADD KEY `ProcessorId` (`ProcessorId`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`Serial`);
+
+--
+-- Indexes for table `gpu`
+--
+ALTER TABLE `gpu`
+  ADD PRIMARY KEY (`GpuId`);
+
+--
+-- Indexes for table `hddandssd`
+--
+ALTER TABLE `hddandssd`
+  ADD PRIMARY KEY (`DriveId`);
+
+--
+-- Indexes for table `monitor`
+--
+ALTER TABLE `monitor`
+  ADD PRIMARY KEY (`MonitorId`);
+
+--
+-- Indexes for table `motherboard`
+--
+ALTER TABLE `motherboard`
+  ADD PRIMARY KEY (`MotherboardId`);
+
+--
+-- Indexes for table `processor`
+--
+ALTER TABLE `processor`
+  ADD PRIMARY KEY (`ProcessorId`);
+
+--
+-- Indexes for table `psu`
+--
+ALTER TABLE `psu`
+  ADD PRIMARY KEY (`PsuId`);
+
+--
+-- Indexes for table `ram`
+--
+ALTER TABLE `ram`
+  ADD PRIMARY KEY (`RamId`);
+
+--
+-- Indexes for table `recommendedshops`
+--
+ALTER TABLE `recommendedshops`
+  ADD PRIMARY KEY (`ShopId`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `benchmark_gpu`
+--
+ALTER TABLE `benchmark_gpu`
+  ADD CONSTRAINT `benchmark_gpu_ibfk_1` FOREIGN KEY (`GpuId`) REFERENCES `gpu` (`GpuId`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `benchmark_processor`
+--
+ALTER TABLE `benchmark_processor`
+  ADD CONSTRAINT `benchmark_processor_ibfk_1` FOREIGN KEY (`ProcessorId`) REFERENCES `processor` (`ProcessorId`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
