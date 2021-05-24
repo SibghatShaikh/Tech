01ce67a43dd32d9c000b687e307e96643cb4e672
SibghatShaikh <79777495+SibghatShaikh@users.noreply.github.com>
2021-05-25 04:11:45 +0600

    Add files via upload

--- Tech/admin.py
+++ Tech/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import*
 # Register your models here.
+
+admin.site.register(Accessories)
+admin.site.register(Gpu)
+admin.site.register(Hddandssd)
+admin.site.register(Monitor)
+admin.site.register(Motherboard)
+admin.site.register(Processor)
+admin.site.register(Psu)
+admin.site.register(Ram)
+admin.site.register(Recommendedshops)
